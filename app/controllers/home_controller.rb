569591968cbb7d94e3153cdf9f1510cdef2009e6
Ronald Coello <ronaldcoello85@gmail.com>
2024-03-16 20:44:06 -0600

    fixed footer

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,14 @@
 class HomeController < ApplicationController
   def index
-    @discussion_threads = DiscussionThread.all
+    @sorting_option = params[:sort_by] || 'date_posted'
+
+    @discussion_threads = DiscussionThread.includes(:likes, :comments).all
+
+    case @sorting_option
+    when 'date_posted'
+      @discussion_threads = @discussion_threads.order(created_at: :desc)
+    when 'relevance'
+      @discussion_threads = @discussion_threads.sort_by { |thread| -(thread.comments.count + thread.likes.count) }
+    end
   end
 end
