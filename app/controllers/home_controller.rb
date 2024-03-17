b44eaf80c09129bb6fbca668eacd67c3a96d5de3
Ronald Coello <ronaldcoello85@gmail.com>
2024-03-16 21:57:06 -0600

    finished

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,13 +2,20 @@ class HomeController < ApplicationController
   def index
     @sorting_option = params[:sort_by] || 'date_posted'
 
-    @discussion_threads = DiscussionThread.includes(:likes, :comments).all
+    # Start with all threads or only current user's threads based on the filter
+    if params[:sort_by] == 'my_threads'
+      @discussion_threads = DiscussionThread.includes(:likes, :comments).where(user: current_user)
+    else
+      @discussion_threads = DiscussionThread.includes(:likes, :comments).all
+    end
 
+    # Apply sorting based on the selected option
     case @sorting_option
     when 'date_posted'
       @discussion_threads = @discussion_threads.order(created_at: :desc)
     when 'relevance'
-      @discussion_threads = @discussion_threads.sort_by { |thread| -(thread.comments.count + thread.likes.count) }
+      # Convert to an array for sorting by relevance since it involves calculation
+      @discussion_threads = @discussion_threads.to_a.sort_by { |thread| -(thread.comments.count + thread.likes.count) }
     end
   end
 end
